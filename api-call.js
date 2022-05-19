7fcaea3c3fa796d332ef6db12af7a626adbb3239
SirGlaucus <glaucusjoseph@gmail.com>
2022-05-18 21:58:52 -0400

    Creando los archivos JSON

--- api-call.js
+++ api-call.js
@@ -1,8 +1,28 @@
 const axios = require('axios')
+const { v4: uuidv4 } = require('uuid')
+const fs = require('fs')
 
-const llamadoApi = async () => {
-    const data = await axios.get('https://mindicador.cl/api')
-    return data
+const nuevoUsuario = async () => {
+    try {
+        const {data} = await axios.get('http://randomuser.me./api')
+        const usuario = data.results[0]
+        const user = {
+            id: uuidv4().slice(30),
+            nombre: `${usuario.name.title} ${usuario.name.first} ${usuario.name.last}`,
+        }
+        return user
+    } catch(e) {
+        throw e
+    }
 }
 
-module.exports = llamadoApi
+const guardarUsuario = (usuario) => {
+    const usuariosJSON = JSON.parse(fs.readFileSync('usuarios.json', 'utf8'))
+    usuariosJSON.usuarios.push(usuario)
+    fs.writeFileSync('usuarios.json', JSON.stringify(usuariosJSON))
+}
+
+module.exports = {
+    nuevoUsuario,
+    guardarUsuario
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+const http = require('http')
+const fs = require('fs')
+
+http.createServer((req, res) => {
+    
+}).listen(3000, console.log('Server ON en el puerto 3000'))
